5001f9f6276940720b874a94bbfc73740c34d1ce
Joel Coakes <jpcoakes@gmail.com>
2025-01-12 14:02:54 -0600

    latest lightningCard from org

--- force-app/main/default/lwc/lightningCard/lightningCard.js
+++ force-app/main/default/lwc/lightningCard/lightningCard.js
@@ -4,4 +4,15 @@ export default class LightningCard extends LightningElement {
   @api title;
   @api body;
   @api footer;
+
+  handleClick(event) {
+    // dispatch custom event to let the parent component know that the button was clicked
+    this.dispatchEvent(
+      new CustomEvent("buttonclick", {
+        bubbles: true,
+        composed: true,
+        detail: `${this.title} and ${this.body}`
+      })
+    );
+  }
 }
