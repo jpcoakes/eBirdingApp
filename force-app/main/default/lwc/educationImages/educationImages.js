0aba3e2f8d5e2e3b85e573518929c5aba7ce06b6
Joel Coakes <jpcoakes@gmail.com>
2025-02-10 10:03:54 -0600

    add education images component and network to repo

---
+++ force-app/main/default/lwc/educationImages/educationImages.js
@@ -0,0 +1,12 @@
+import { LightningElement } from "lwc";
+import auburn from "@salesforce/resourceUrl/auburnLogo";
+import purdue from "@salesforce/resourceUrl/purdueLogo";
+import cca from "@salesforce/resourceUrl/ccaLogo";
+import memphis from "@salesforce/resourceUrl/memphisLogo";
+
+export default class EducationImages extends LightningElement {
+  auburnUrl = auburn;
+  purdueUrl = purdue;
+  ccaUrl = cca;
+  memphisUrl = memphis;
+}
